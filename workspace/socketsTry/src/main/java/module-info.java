6e38c5317cdb8075921ee33e1d6d66a068219814
Tourment0412 <santiquinterouribe0412@gmail.com>
2023-10-08 00:17:49 -0500

    Created base maven project for socketsTry

---
+++ workspace/socketsTry/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+open module co.edu.uniquindio.programacionIII.socketsTry {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.swing;
+
+
+    exports co.edu.uniquindio.programacionIII.socketsTry;
+}
