19836dc377e3a6fc0d0b3c90d5fce13561b5c14b
Tourment0412 <santiquinterouribe0412@gmail.com>
2023-10-08 00:18:42 -0500

    main moved

--- workspace/socketsTry/src/main/java/co/edu/uniquindio/programacionIII/socketsTry/App.java
+++ workspace/socketsTry/src/main/java/co/edu/uniquindio/programacionIII/socketsTry/App.java
@@ -14,6 +14,10 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
+    
+    public static void main(String[] args) {
+        launch();
+    }
 
     @Override
     public void start(Stage stage) throws IOException {
@@ -31,8 +35,6 @@ public class App extends Application {
         return fxmlLoader.load();
     }
 
-    public static void main(String[] args) {
-        launch();
-    }
+   
 
 }
